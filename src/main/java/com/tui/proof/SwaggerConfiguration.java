b5221785280eed2bb8cf900a7126ec787ea2016d
przeswid <przemek_swida@yahoo.pl>
2021-11-09 13:29:56 +0100

    Added OpenAPI description.

--- src/main/java/com/tui/proof/SwaggerConfiguration.java
+++ src/main/java/com/tui/proof/SwaggerConfiguration.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -18,6 +22,17 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/order/**"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Order \"pilotes\" API",
+                "API REST that allows to order the \"pilotes\" of the great Miquel Montoro.",
+                "v1_0",
+                null,
+                new Contact("Przemek Świda", "", "przemyslaw.swida@boldare.com"),
+                "Apache License 2.0", "https://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
     }
 }
